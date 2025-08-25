800e418d2d8d1004eb94fea57d18d37bd858624f
Ryan Yogan <ryanyogan@gmail.com>
2025-08-25 09:06:16 -0500

    screen shots, workflows, more

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -39,3 +39,17 @@ App.get('/:id', async (c) => {
 
 	return c.redirect(destination);
 });
+
+App.get('/do/:name', async (c) => {
+	const name = c.req.param('name');
+	const doId = c.env.EVALUATION_SCHEDULER.idFromName(name);
+	const stub = c.env.EVALUATION_SCHEDULER.get(doId);
+
+	await stub.increment();
+
+	const count = await stub.getCount();
+
+	return c.json({
+		count,
+	});
+});
--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -1,13 +1,54 @@
+import { aiDestinationChecker } from '@/helpers/ai-destination-checker';
+import { collectDestinationInfo } from '@/helpers/browser-render';
+import { initDatabase } from '@repo/data-ops/database';
+import { addEvalution } from '@repo/data-ops/queries/evaluationts';
 import { WorkflowEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers';
 
-export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, unknown> {
-	async run(event: Readonly<WorkflowEvent<unknown>>, step: WorkflowStep) {
+export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
+	async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep) {
+		initDatabase(this.env.DB);
+
 		const collectionData = await step.do('Collect rendered destination page data', async () => {
-			return {
-				data: 'Hello World',
-				payload: `Event: ${event.payload}`,
-			};
+			return collectDestinationInfo(this.env, event.payload.destinationUrl);
 		});
+
+		const aiStatus = await step.do(
+			'Use AI to check the status of the page',
+			{
+				retries: {
+					limit: 0,
+					delay: 0,
+				},
+			},
+			async () => {
+				return await aiDestinationChecker(this.env, collectionData.bodyText);
+			},
+		);
+
+		const evaluationId = await step.do('Save evaluation in database', async () => {
+			return await addEvalution({
+				linkId: event.payload.linkId,
+				status: aiStatus.status,
+				reason: aiStatus.statusReason,
+				accountId: event.payload.accountId,
+				destinationUrl: event.payload.destinationUrl,
+			});
+		});
+
+		await step.do('Backup destintion HTML in R2', async () => {
+			const accountId = event.payload.accountId;
+			const r2PathHtml = `evaulations/${accountId}/html/${evaluationId}`;
+			const r2PathBodyText = `evaulations/${accountId}/body-text/${evaluationId}`;
+			const r2PathScreenshot = `evaluation/${accountId}/screenshots/${evaluationId}.png`;
+
+			const screenshotBase64 = collectionData.screenshotDataUrl.replace(/^data:image\/png;base64,/, '');
+			const screenshotBuffer = Buffer.from(screenshotBase64, 'base64');
+
+			await this.env.BUCKET.put(r2PathHtml, collectionData.html);
+			await this.env.BUCKET.put(r2PathBodyText, collectionData.bodyText);
+			await this.env.BUCKET.put(r2PathScreenshot, screenshotBuffer);
+		});
+
 		console.log(collectionData);
 	}
 }
