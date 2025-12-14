17baeacf9d458c75a9ce993212c36d76d60ec481
Matthew Sessions <matthewlsessions@gmail.com>
2025-12-14 08:20:11 -1000

    added db cache

--- packages/data-ops/src/db/database.ts
+++ packages/data-ops/src/db/database.ts
@@ -2,7 +2,14 @@ import { drizzle } from "drizzle-orm/d1";
 
 let db: ReturnType<typeof drizzle>;
 
+/**
+ * Sets up the database connection. Don't double initialize when a worker is hot
+ * and has already been initialized.
+ * @param bindingDb
+ * @returns
+ */
 export function initDatabase(bindingDb: D1Database) {
+  if (db) return;
   db = drizzle(bindingDb);
 }
 
